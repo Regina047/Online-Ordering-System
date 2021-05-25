a1768741593f6bacaec60388e80a9fafa38af639
chabbie1202 <82546296+chabbie1202@users.noreply.github.com>
2021-05-25 11:37:56 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,16 +1,27 @@
+<?php
+
+$active='Shop';
+include("includes/db_conn.php");
+
+
+
+ ?>
+
 <div class="panel panel-default sidebar-menu">
-    <div class="panel-heading">
-        <h3 class="panel-title">Products Categories</h3>
-    </div>
+	<div class="panel-heading">
+		<h3 class="panel-title">Categories</h3>
+	</div>
 
-    <div class="panel-body">
-        <ul class="nav nav-pills nav-stacked category-menu">
-            <li><a href="#">Dish</a></li>
-            <li><a href="#">Drinks</a></li>
-            <li><a href="#">Dessert</a></li>
-            <li><a href="#">Delicacies</a></li>
-            <li><a href="#">Soup</a></li>
-        </ul>
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			
+			 	<?php
 
-    </div>
+			getPCats();
+
+			 ?>
+			
+		</ul>
+	</div>
+
 </div>
