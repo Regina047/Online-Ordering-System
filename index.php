5f49c072d91d76fa757ae457df1f741c5dfe3cc3
chabbie1202 <82546296+chabbie1202@users.noreply.github.com>
2021-05-25 11:35:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,35 @@
 <?php
-include_once "includes/db_conn.php";
-//include"includes/db_conn.php";
-include_once "functions/function.php";
 
-?>
+$active='Home';
+include("includes/db_conn.php");
+
+
+
+ ?>
+
 
 
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Iconicplate store || PHP</title>
+	<link rel="stylesheet"  href="styles/bootstrap-337.min.css">
+	<link rel="stylesheet"  href="styles/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Iconic Plate</title>
-
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="style.css">
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="file:///C:/Users/admin/Downloads/bootstrap-5.0.0-beta3-dist/bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css">
-
-		<script type="text/javascript" src="file:///C:/Users/admin/Downloads/bootstrap-5.0.0-beta3-dist/bootstrap-5.0.0-beta3-dist/js/bootstrap.min.js"></script> -->
 </head>
-
 <body>
-    <div><?php require_once "header.php"?></div>
-    <div><?php require_once "promotion.php"?></div>
-    <div><?php require_once "latest_product.php"?></div>
-    <div><?php require_once "footer.php"?></div>
+	
+	
+	<?php include ("header.php"); ?>
+	<?php include ("slider.php"); ?>
 
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
+	<?php include ("latest_product.php"); ?>
+	<?php include ("footer.php"); ?>
 
+	<script src="js/bootstrap-337.min"></script>
+	<script src="js/jquery-331.min.js"></script>
 </body>
-
 </html>
-
-
-
-<!-- <html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-		
-		<title>Project Sample</title>
-		<link rel="stylesheet" href="style.css">
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-	</head>
-	<body>
-		<div><?php //require "header.php"?></div> 
-		<div><?php //require "promotion.php"?></div>
-		<div><?php //require "latest-product.php"?></div>
-		<div><?php //require "footer.php"?></div>
-		
-
-		
-
-		
-
-	</body>
-</html> -->
